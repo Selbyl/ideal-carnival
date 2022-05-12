363109b45659334eacec15520a09609325b5cb69
Selbyl <selbyclebert@gmail.com>
2022-05-12 15:32:17 -0500

    Updated main.py
    Created 10 second delay to avoid error 429

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import tweepy
 import time
 import csv
 while True:
+    time.sleep(10)
     client = tweepy.Client(bearer_token='Bearer Token')
     api = tweepy.API(client, wait_on_rate_limit=True, timeout=60, retry_count=10, retry_delay=30)
 
@@ -10,7 +11,7 @@ while True:
 
     tweets = client.search_recent_tweets(query=query, tweet_fields=['context_annotations', 'created_at'],
                                      media_fields=['preview_image_url'], expansions='attachments.media_keys',
-                                     max_results=10)
+                                     max_results=100)
 
 # Get list of media from the includes object
     media = {m["media_key"]: m for m in tweets.includes['media']}
@@ -22,4 +23,3 @@ while True:
         print(tweet.text)
         if media[media_keys[0]].preview_image_url:
              print(media[media_keys[0]].preview_image_url)
-sleep.sleep(60)
